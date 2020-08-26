78ff8d37b9c2ebd85a09095b17eb26ee3bed590a
kalh <kalh@TOWER.home>
2020-08-26 23:25:21 +0200

    fix

--- Watchfaces/HRCanvasWatch/lib/weather/vForecast.class.js
+++ Watchfaces/HRCanvasWatch/lib/weather/vForecast.class.js
@@ -2,7 +2,7 @@ class vForecast {
 	constructor(array){
 		this.date = new Date(array['dt']*1000);
 		this.main = array['main'];
-		this.weather = array['weather'];
+		this.weather = array['weather'][0];
 		this.wind = array['wind'];
 		this.day = array['day'];
 
